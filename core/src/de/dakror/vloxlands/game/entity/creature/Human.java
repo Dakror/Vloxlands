b06dd3a35c4e74af14bf9b7c6fb3d5e90388ffbf
Dakror <mail@dakror.de>
2014-05-04 15:17:53 +0200

    Now it's fixed

--- core/src/de/dakror/vloxlands/game/entity/creature/Human.java
+++ core/src/de/dakror/vloxlands/game/entity/creature/Human.java
@@ -3,22 +3,20 @@ package de.dakror.vloxlands.game.entity.creature;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.bullet.collision.btBoxShape;
 
-import de.dakror.vloxlands.game.entity.Entity;
-
 
 /**
  * @author Dakror
  */
-public class Human extends Entity
+public class Human extends Creature
 {
 	public Human(float x, float y, float z)
 	{
 		super(x, y, z, new Vector3(), "models/humanblend/humanblend.g3db");
 		name = "Mensch";
 		
-		// speed = 0.05f;
-		// climbHeight = 1;
-		// middleTranslate = -0.45f;
+		speed = 0.05f;
+		climbHeight = 1;
+		middleTranslate = -0.45f;
 		
 		Vector3 v = boundingBox.getDimensions().cpy().scl(0.5f);
 		btBoxShape bt = new btBoxShape(new Vector3(v.x, v.z, v.y));
