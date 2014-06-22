e972e375c31ebefe1e52912876bc7e12568ffc3c
Dakror <mail@dakror.de>
2014-06-22 11:38:04 +0200

    Minimap selection fixed

--- core/src/de/dakror/vloxlands/ui/Minimap.java
+++ core/src/de/dakror/vloxlands/ui/Minimap.java
@@ -2,7 +2,10 @@ package de.dakror.vloxlands.ui;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 
 import de.dakror.vloxlands.Vloxlands;
@@ -42,6 +45,24 @@ public class Minimap extends Group
 				addActor(mi);
 			}
 		}
+		
+		addListener(new InputListener()
+		{
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button)
+			{
+				for (Actor a : getChildren())
+					if (a instanceof MinimapIsland) ((MinimapIsland) a).active = false;
+				
+				Actor actor = hit(x, y, true);
+				if (actor != null && actor instanceof MinimapIsland)
+				{
+					((MinimapIsland) actor).active = true;
+					GameLayer.instance.focusIsland(((MinimapIsland) actor).island, false);
+				}
+				return false;
+			}
+		});
 	}
 	
 	@Override
