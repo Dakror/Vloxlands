53ab54005ff56f760439fb0fc72bf05fc049a613
Dakror <mail@dakror.de>
2014-06-25 19:03:34 +0200

    DebugLayer crashfix

--- core/src/de/dakror/vloxlands/layer/DebugLayer.java
+++ core/src/de/dakror/vloxlands/layer/DebugLayer.java
@@ -25,11 +25,14 @@ public class DebugLayer extends Layer
 		spriteBatch.begin();
 		
 		font.draw(spriteBatch, "FPS: " + Gdx.graphics.getFramesPerSecond(), 0, Gdx.graphics.getHeight());
-		font.draw(spriteBatch, "C: " + GameLayer.world.visibleChunks + " / " + GameLayer.world.loadedChunks + " / " + GameLayer.world.chunks, 0, Gdx.graphics.getHeight() - 20);
-		font.draw(spriteBatch, "E: " + GameLayer.world.visibleEntities + " / " + GameLayer.world.totalEntities, 0, Gdx.graphics.getHeight() - 40);
-		font.draw(spriteBatch, "X: " + GameLayer.camera.position.x, 0, Gdx.graphics.getHeight() - 60);
-		font.draw(spriteBatch, "Y: " + GameLayer.camera.position.y, 0, Gdx.graphics.getHeight() - 80);
-		font.draw(spriteBatch, "Z: " + GameLayer.camera.position.z, 0, Gdx.graphics.getHeight() - 100);
+		if (GameLayer.world != null)
+		{
+			font.draw(spriteBatch, "C: " + GameLayer.world.visibleChunks + " / " + GameLayer.world.loadedChunks + " / " + GameLayer.world.chunks, 0, Gdx.graphics.getHeight() - 20);
+			font.draw(spriteBatch, "E: " + GameLayer.world.visibleEntities + " / " + GameLayer.world.totalEntities, 0, Gdx.graphics.getHeight() - 40);
+			font.draw(spriteBatch, "X: " + GameLayer.camera.position.x, 0, Gdx.graphics.getHeight() - 60);
+			font.draw(spriteBatch, "Y: " + GameLayer.camera.position.y, 0, Gdx.graphics.getHeight() - 80);
+			font.draw(spriteBatch, "Z: " + GameLayer.camera.position.z, 0, Gdx.graphics.getHeight() - 100);
+		}
 		font.draw(spriteBatch, "Seed: " + GameLayer.seed, 0, Gdx.graphics.getHeight() - 120);
 		spriteBatch.end();
 	}
