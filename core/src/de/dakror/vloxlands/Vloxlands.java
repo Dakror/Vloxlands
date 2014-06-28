884de7ab2c2cfc74e02a7dcc8c2b2a02ad1d1a1e
Dakror <mail@dakror.de>
2014-06-28 22:11:29 +0200

    Consistent save game name

--- core/src/de/dakror/vloxlands/Vloxlands.java
+++ core/src/de/dakror/vloxlands/Vloxlands.java
@@ -29,54 +29,54 @@ public class Vloxlands extends GameBase
 	public static Vloxlands currentGame;
 	public static AssetManager assets;
 	public static Skin skin;
-	
+
 	long last;
 	int tick;
-	
+
 	public static boolean showPathDebug;
-	
+
 	@Override
 	public void create()
 	{
 		Gdx.app.setLogLevel(Application.LOG_DEBUG);
-		
+
 		currentGame = this;
-		
+
 		Config.init();
 		setFullscreen(Config.pref.getBoolean("fullscreen"));
 		Voxel.loadVoxels();
 		Item.loadItems();
-		
+
 		assets = new AssetManager();
 		skin = new Skin(Gdx.files.internal("skin/default/uiskin.json"));
-		
+
 		getMultiplexer().addProcessor(0, new GestureDetector(this));
 		getMultiplexer().addProcessor(0, this);
 		Gdx.input.setInputProcessor(getMultiplexer());
-		
+
 		addLayer(new LoadingLayer());
 	}
-	
+
 	@Override
 	public void render()
 	{
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-		
+
 		for (Layer l : layers)
 			l.render(Gdx.graphics.getDeltaTime());
-		
+
 		if (last == 0) last = System.currentTimeMillis();
-		
+
 		if (System.currentTimeMillis() - last >= 16) // ~60 a sec
 		{
 			tick++;
-			
+
 			for (Layer l : layers)
 				l.tick(tick);
 			last = System.currentTimeMillis();
 		}
 	}
-	
+
 	@Override
 	public boolean keyUp(int keycode)
 	{
@@ -90,34 +90,41 @@ public class Vloxlands extends GameBase
 		if (keycode == Keys.F11)
 		{
 			setFullscreen(!Gdx.graphics.isFullscreen());
-			
+
 			return true;
 		}
 		if (keycode == Keys.F6 && GameLayer.world != null)
 		{
 			try
 			{
+				boolean wasNull = false;
+				if (Config.savegameName == null)
+				{
+					wasNull = true;
+					Config.savegameName = new SimpleDateFormat("dd.MM.yy HH-mm-ss").format(new Date());
+				}
+				
 				ByteArrayOutputStream baos = new ByteArrayOutputStream();
 				GameLayer.world.save(baos);
-				FileHandle file = Gdx.files.external(".dakror/Vloxlands/maps/" + new SimpleDateFormat("dd.MM.yy HH-mm-ss").format(new Date()) + ".map");
+				FileHandle file = Gdx.files.external(".dakror/Vloxlands/maps/" + Config.savegameName + ".map");
 				file.writeBytes(Compressor.compress(baos.toByteArray()), false);
-				Gdx.app.log("Vloxlands.keyUp", "Game saved as " + file.name() + " (" + MathHelper.formatBinarySize(file.length(), 0) + ")");
+				Gdx.app.log("Vloxlands.keyUp", "Game saved" + (wasNull ? " as " + file.name() + " (" + MathHelper.formatBinarySize(file.length(), 0) + ")." : "."));
 			}
 			catch (IOException e)
 			{
 				e.printStackTrace();
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 	@Override
 	public void pause()
 	{
 		Config.savePrefs();
 	}
-	
+
 	public void setFullscreen(boolean fullscreen)
 	{
 		if (!fullscreen) Gdx.graphics.setDisplayMode(1280, 720, false);
