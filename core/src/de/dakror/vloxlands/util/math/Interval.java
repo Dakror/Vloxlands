92b0f7ce836403524118ee4447324d62a71eded5
Dakror <mail@dakror.de>
2014-06-29 22:47:54 +0200

    minor improvements

--- core/src/de/dakror/vloxlands/util/math/Interval.java
+++ core/src/de/dakror/vloxlands/util/math/Interval.java
@@ -12,17 +12,17 @@ public class Interval implements Comparable<Interval>, Savable
 {
 	int start, length;
 	byte type;
-
+	
 	public Interval()
 	{}
-
+	
 	public Interval(byte type, int start, int length)
 	{
 		this.type = type;
 		this.start = start;
 		this.length = length;
 	}
-
+	
 	public Interval set(byte type, int start, int length)
 	{
 		this.type = type;
@@ -30,63 +30,63 @@ public class Interval implements Comparable<Interval>, Savable
 		this.length = length;
 		return this;
 	}
-	
+
 	public byte getType()
 	{
 		return type;
 	}
-
+	
 	public void setType(byte type)
 	{
 		this.type = type;
 	}
-	
+
 	public void shrink()
 	{
 		length--;
 	}
-
+	
 	public void extend()
 	{
 		length++;
 	}
-
+	
 	public int getStart()
 	{
 		return start;
 	}
-
+	
 	public void setStart(int start)
 	{
 		this.start = start;
 	}
-
+	
 	public int getLength()
 	{
 		return length;
 	}
-
+	
 	public void setLength(int length)
 	{
 		this.length = length;
 	}
-
+	
 	@Override
 	public int compareTo(Interval o)
 	{
 		return Integer.compare(start, o.start);
 	}
-	
+
 	@Override
 	public String toString()
 	{
 		return start + "-[" + type + "]-" + (start + length);
 	}
-
+	
 	@Override
 	public void save(ByteArrayOutputStream baos) throws IOException
 	{
-		Bits.putShort(baos, (short) length);
+		Bits.putInt(baos, length);
 		baos.write(type);
 	}
 }
--- core/src/test/IntervalTreeTest.java
+++ core/src/test/IntervalTreeTest.java
@@ -16,36 +16,47 @@ public class IntervalTreeTest
 {
 	static SplayTree<Interval> tree;
 	static final Interval tmp = new Interval();
-	
-	
+
+	static float ab = 0;
+	static float ac = 0;
+
 	static final String TYPES = "abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-	
+
 	/**
 	 * @param args
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException
 	{
-		tree = SplayTree.create();
+		for (int ind = 0; ind < 20; ind++)
+		{
+			tree = SplayTree.create();
+			
+			tree.insert(new Interval((byte) 0, 0, 256 * 256 * 256));
+			
+			int muts = MathUtils.random(0, 1024);
+			System.out.println(muts);
+			for (int i = 0; i < muts; i++)
+			{
+				int x = MathUtils.random(0, 256 * 256 * 256 - 1);
+				byte b = (byte) MathUtils.random(TYPES.length() - 1);
+				set(x, b);
+			}
+			
+			b();
+		}
 		
-		tree.insert(new Interval((byte) 0, 0, 256 * 256));
-		
-		int muts = MathUtils.random(0, 1024);
-		System.out.println(muts);
-		for (int i = 0; i < muts; i++)
-			set(MathUtils.random(0, 256 * 256 - 1), (byte) MathUtils.random(TYPES.length() - 1));
-		
-		b();
+		System.out.println(ab + ", " + ac);
 	}
-
+	
 	public static void set(int x, byte b)
 	{
 		Interval i = search(x);
 		Interval j = null;
 		if (i == null) throw new IllegalArgumentException();
-
+		
 		if (i.getType() == b) return;
-
+		
 		if (i.getLength() == 0)
 		{
 			i.setType(b);
@@ -54,7 +65,7 @@ public class IntervalTreeTest
 		{
 			i.setStart(x + 1);
 			i.shrink();
-
+			
 			j = search(x - 1);
 			if (j != null && j.getType() == b) j.extend();
 			else tree.insert(new Interval(b, x, 0));
@@ -62,7 +73,7 @@ public class IntervalTreeTest
 		else if (i.getStart() + i.getLength() == x)
 		{
 			i.shrink();
-
+			
 			j = search(x + 1);
 			if (j != null && j.getType() == b)
 			{
@@ -80,14 +91,14 @@ public class IntervalTreeTest
 			tree.insert(new Interval(i.getType(), x + 1, length - 1));
 		}
 	}
-
+	
 	public static byte get(int x)
 	{
 		Interval i = search(x);
 		if (i == null) throw new IllegalArgumentException();
 		return i.getType();
 	}
-
+	
 	public static Interval search(int x)
 	{
 		for (int i = x; i > -1; i--)
@@ -95,34 +106,44 @@ public class IntervalTreeTest
 			tmp.set((byte) -1, i, 1);
 			if (tree.contains(tmp)) return tree.find(tmp);
 		}
-
+		
 		return null;
 	}
-
+	
 	public static void p()
 	{
 		for (Interval i : tree)
 			System.out.print(i + ",");
 		System.out.println();
 	}
-
+	
 	public static void p2()
 	{
 		for (Interval i : tree)
 			System.out.print((i.getLength() + 1) + "" + TYPES.charAt(i.getType()));
 		System.out.println();
 	}
-
+	
 	public static void b() throws IOException
 	{
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		for (Interval i : tree)
 			i.save(baos);
-
-		System.out.println(baos.size());
+		
 		byte[] b = Compressor.compress(baos.toByteArray());
 		byte[] c = Compressor.compressRow(baos.toByteArray());
-		System.out.println(b.length);
-		System.out.println(c.length);
+
+		float abb = b.length / (float) baos.size() * 100f;
+		float acc = c.length / (float) baos.size() * 100f;
+
+
+
+		if (ab == 0) ab = abb;
+		else ab = (ab + abb) / 2f;
+
+		if (ac == 0) ac = acc;
+		else ac = (ac + acc) / 2f;
+
+		// System.out.println(abb + ", " + acc);
 	}
 }
