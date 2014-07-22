1e83b279b922ffb1d4f5358753213438991796d5
Dakror <mail@dakror.de>
2014-07-22 21:55:12 +0200

    Android setup again

---
+++ android/src/de/dakror/vloxlands/android/AndroidLauncher.java
@@ -0,0 +1,19 @@
+package de.dakror.vloxlands.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+import de.dakror.vloxlands.Vloxlands;
+
+public class AndroidLauncher extends AndroidApplication
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Vloxlands(), config);
+	}
+}
