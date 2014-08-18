0a16843ba2af17a30494464d3c379aae2eae0d9a
Dakror <mail@dakror.de>
2014-08-18 11:35:35 +0200

    removed ugly surface stones

--- core/src/de/dakror/vloxlands/generate/biome/ModerateBiome.java
+++ core/src/de/dakror/vloxlands/generate/biome/ModerateBiome.java
@@ -21,9 +21,8 @@ public class ModerateBiome extends Biome
 	@Override
 	public void generate(WorldGenerator worldGen, Island island, int radius)
 	{
-		byte[] tlRatio = createRatio(new byte[] { Voxel.get("DIRT").getId(), Voxel.get("STONE").getId() }, new int[] { 30, 1 });
 		int j = (int) (3 + 3 * MathUtils.random() + radius / 8f);
-		generateBezier(island, Beziers.TOPLAYER, Island.SIZE / 2, Island.SIZE / 2, radius, Island.SIZE / 4 * 3, j, tlRatio, true);
+		generateBezier(island, Beziers.TOPLAYER, Island.SIZE / 2, Island.SIZE / 2, radius, Island.SIZE / 4 * 3, j, new byte[] { Voxel.get("DIRT").getId() }, true);
 		worldGen.step();
 		
 		byte[] sRatio = createRatio(new byte[] { Voxel.get("STONE").getId(), Voxel.get("DIRT").getId() }, new int[] { 5, 1 });
