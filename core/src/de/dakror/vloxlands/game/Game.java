64a7474c8492c17e9d72af8f8ece902f82480dc6
Dakror <mail@dakror.de>
2014-09-14 15:13:41 +0200

    Wheat growth implemented

--- core/src/de/dakror/vloxlands/game/Game.java
+++ core/src/de/dakror/vloxlands/game/Game.java
@@ -161,7 +161,29 @@ public class Game extends Layer
 			protected boolean process(float deltaX, float deltaY, int button)
 			{
 				if (button == rotateButton && Gdx.input.isKeyPressed(Keys.CONTROL_LEFT)) return false;
-				return super.process(deltaX, deltaY, button);
+				
+				if (button == rotateButton)
+				{
+					tmpV1.set(camera.direction).crs(camera.up).y = 0f;
+					camera.rotateAround(target, tmpV1.nor(), deltaY * rotateAngle);
+					
+					float dot = camera.direction.dot(Vector3.Y);
+					if (dot < -0.95f || dot > 0.5f) camera.rotateAround(target, tmpV1.nor(), -deltaY * rotateAngle);
+					camera.rotateAround(target, Vector3.Y, deltaX * -rotateAngle);
+				}
+				else if (button == translateButton)
+				{
+					camera.translate(tmpV1.set(camera.direction).crs(camera.up).nor().scl(-deltaX * translateUnits));
+					camera.translate(tmpV2.set(camera.up).scl(-deltaY * translateUnits));
+					if (translateTarget) target.add(tmpV1).add(tmpV2);
+				}
+				else if (button == forwardButton)
+				{
+					camera.translate(tmpV1.set(camera.direction).scl(deltaY * translateUnits));
+					if (forwardTarget) target.add(tmpV1);
+				}
+				if (autoUpdate) camera.update();
+				return true;
 			}
 			
 			@Override
--- core/src/de/dakror/vloxlands/game/entity/statics/Wheat.java
+++ core/src/de/dakror/vloxlands/game/entity/statics/Wheat.java
@@ -3,7 +3,15 @@ package de.dakror.vloxlands.game.entity.statics;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.math.Matrix4;
+
+import de.dakror.vloxlands.Vloxlands;
+import de.dakror.vloxlands.ai.state.StateTools;
 import de.dakror.vloxlands.game.Game;
+import de.dakror.vloxlands.game.voxel.MetaTags;
+import de.dakror.vloxlands.game.voxel.Voxel;
 import de.dakror.vloxlands.util.math.Bits;
 
 /**
@@ -19,44 +27,50 @@ public class Wheat extends StaticEntity
 	
 	public static int itemsForHarvest = 5;
 	
+	
 	public Wheat(float x, float y, float z)
 	{
-		this(x, y, z, 0);
-	}
-	
-	public Wheat(float x, float y, float z, int phase)
-	{
-		super(x, y, z, "entities/wheat/wheat" + phase + "[16].vxi");
-		growTicks = growTicksLeft = Game.dayInTicks / 3 * 2;
+		super(x, y, z, "entities/wheat/wheat0[16].vxi");
+		growTicks = growTicksLeft = Game.dayInTicks;
 		perLevel = growTicks / 5;
 	}
 	
-	// @Override
-	// public void onSpawn()
-	// {
-	// super.onSpawn();
-	//
-	// level = 0;
-	// island.set(voxelPos.x, voxelPos.y, voxelPos.z, Voxel.get("ACRE").getId());
-	// island.setMeta(voxelPos.x, voxelPos.y, voxelPos.z, MetaTags.ACRE_PLANT_GROWING);
-	// }
+	@Override
+	public void onSpawn()
+	{
+		super.onSpawn();
+		
+		level = 0;
+		island.set(voxelPos.x, voxelPos.y, voxelPos.z, Voxel.get("ACRE").getId());
+		island.setMeta(voxelPos.x, voxelPos.y, voxelPos.z, MetaTags.ACRE_PLANT_GROWING);
+	}
 	
 	@Override
 	public void tick(int tick)
 	{
 		super.tick(tick);
 		
-		// if (level < 4)
-		// {
-		// if (StateTools.isWorkingTime()) growTicksLeft--; // only grows in sunlight, so <code>initial time * 2</code> = real time is takes
-		//
-		// if (growTicksLeft % perLevel == 0)
-		// {
-		// level++;
-		// modelInstance = new ModelInstance(Vloxlands.assets.get("models/" + "entities/wheat/wheat" + level + "[16].vxi", Model.class));
-		// }
-		// if (level == 4) island.setMeta(voxelPos.x, voxelPos.y, voxelPos.z, MetaTags.ACRE_PLANT_GROWN);
-		// }
+		if (level < 4)
+		{
+			if (StateTools.isWorkingTime()) growTicksLeft--; // only grows in sunlight, so <code>initial time * 2</code> = real time is takes
+			
+			if (growTicksLeft % perLevel == 0 && growTicksLeft != growTicks)
+			{
+				level++;
+				Matrix4 tr = modelInstance.transform.cpy().translate(-blockTrn.x, -blockTrn.y, -blockTrn.z);
+				modelInstance = new ModelInstance(Vloxlands.assets.get("models/" + "entities/wheat/wheat" + level + "[16].vxi", Model.class));
+				modelInstance.transform.set(tr);
+				modelInstance.calculateBoundingBox(boundingBox);
+				if (boundingBox.getDimensions().x <= 1 || boundingBox.getDimensions().y <= 1 || boundingBox.getDimensions().z <= 1)
+				{
+					blockTrn.set(((float) Math.ceil(boundingBox.getDimensions().x) - boundingBox.getDimensions().x) / 2, 1 - boundingBox.getCenter().y, ((float) Math.ceil(boundingBox.getDimensions().z) - boundingBox.getDimensions().z) / 2);
+				}
+				blockTrn.add(boundingBox.getDimensions().cpy().scl(0.5f));
+				modelInstance.transform.translate(blockTrn);
+			}
+			
+			if (level == 4) island.setMeta(voxelPos.x, voxelPos.y, voxelPos.z, MetaTags.ACRE_PLANT_GROWN);
+		}
 	}
 	
 	@Override
